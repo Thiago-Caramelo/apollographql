413b87a79b2d8770dd415c9fbac1f08082f317ff
Thiago Caramelo <thiago.pescado@gmail.com>
2019-01-17 10:16:39 -0200

    refactor index code

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
 const { importSchema } = require('graphql-import')
 const resolvers = require('./resolvers')
-
-const typeDefs = importSchema('schema.graphql')
-
-const server = new ApolloServer({ typeDefs, resolvers })
-
-const app = express()
-server.applyMiddleware({ app })
-
 const port = 4000
+const typeDefs = importSchema('schema.graphql')
+const server = new ApolloServer({ typeDefs, resolvers })
+const app = express()
+
+server.applyMiddleware({ app })
 
 app.listen({ port }, () =>
   console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`),
